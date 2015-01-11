bfc73f5076a1167d046494e782fa72b68148cbfa
Ciomirtan Andrei <refublog@gmail.com>
2015-01-11 13:43:03 +0200

    Update tank_auth_lang.php

--- romanian/tank_auth_lang.php
+++ romanian/tank_auth_lang.php
@@ -1,39 +1,39 @@
 <?php
 
 // Errors
-$lang['auth_incorrect_password'] = 'Incorrect password';
-$lang['auth_incorrect_login'] = 'Incorrect login';
-$lang['auth_incorrect_email_or_username'] = 'Login or email doesn\'t exist';
-$lang['auth_email_in_use'] = 'Email is already used by another user. Please choose another email.';
-$lang['auth_username_in_use'] = 'Username already exists. Please choose another username.';
-$lang['auth_current_email'] = 'This is your current email';
-$lang['auth_incorrect_captcha'] = 'Your confirmation code does not match the one in the image.';
-$lang['auth_captcha_expired'] = 'Your confirmation code has expired. Please try again.';
+$lang['auth_incorrect_password'] = 'Parola este incorectă';
+$lang['auth_incorrect_login'] = 'Datele de login incorecte';
+$lang['auth_incorrect_email_or_username'] = 'Datele de login sau email nu există';
+$lang['auth_email_in_use'] = 'Email-ul este deja folosit de un alt utilizator. Te rugam sa alegi o altă adresă de email.';
+$lang['auth_username_in_use'] = 'Numele de utilizator există. Te rugăm să alegi un alt nume de utilizator.';
+$lang['auth_current_email'] = 'Aceasta este adresa ta de email pe care o foloseşti';
+$lang['auth_incorrect_captcha'] = 'Codul tău de confirmare nu corespunde cu cel din imagine.';
+$lang['auth_captcha_expired'] = 'Codul tău de confirmare a expirat. Te rugăm să încearci din nou.';
 
 // Notifications
-$lang['auth_message_logged_out'] = 'You have been successfully logged out.';
-$lang['auth_message_registration_disabled'] = 'Registration is disabled.';
-$lang['auth_message_registration_completed_1'] = 'You have successfully registered. Check your email address to activate your account.';
-$lang['auth_message_registration_completed_2'] = 'You have successfully registered.';
-$lang['auth_message_activation_email_sent'] = 'A new activation email has been sent to %s. Follow the instructions in the email to activate your account.';
-$lang['auth_message_activation_completed'] = 'Your account has been successfully activated.';
-$lang['auth_message_activation_failed'] = 'The activation code you entered is incorrect or expired.';
-$lang['auth_message_password_changed'] = 'Your password has been successfully changed.';
-$lang['auth_message_new_password_sent'] = 'An email with instructions for creating a new password has been sent to you.';
-$lang['auth_message_new_password_activated'] = 'You have successfully reset your password';
-$lang['auth_message_new_password_failed'] = 'Your activation key is incorrect or expired. Please check your email again and follow the instructions.';
-$lang['auth_message_new_email_sent'] = 'A confirmation email has been sent to %s. Follow the instructions in the email to complete this change of email address.';
-$lang['auth_message_new_email_activated'] = 'You have successfully changed your email';
-$lang['auth_message_new_email_failed'] = 'Your activation key is incorrect or expired. Please check your email again and follow the instructions.';
-$lang['auth_message_banned'] = 'You are banned.';
-$lang['auth_message_unregistered'] = 'Your account has been deleted...';
+$lang['auth_message_logged_out'] = 'Ai fost delogat cu succes.';
+$lang['auth_message_registration_disabled'] = 'Înregistrarea este dezactivată.';
+$lang['auth_message_registration_completed_1'] = 'Ai fost înregistrat cu succes. Verifică căsuţa de email petru activarea contului.';
+$lang['auth_message_registration_completed_2'] = 'Ai fost înregistrat cu succes.';
+$lang['auth_message_activation_email_sent'] = 'Un nou mail de activare a fost trimis către adresa de email %s. Urmăreşte informaţiile din email pentru activarea contului.';
+$lang['auth_message_activation_completed'] = 'Contul tău a fost activat cu succes.';
+$lang['auth_message_activation_failed'] = 'Codul de activare introdus a expirat sau nu este corect.';
+$lang['auth_message_password_changed'] = 'Parola ta a fost schimbată cu succes.';
+$lang['auth_message_new_password_sent'] = 'Un email cu instrucţiuni pentru a putea reseta parola a fost trimis către dvs..';
+$lang['auth_message_new_password_activated'] = 'Parola ta a fost resetată cu succes';
+$lang['auth_message_new_password_failed'] = 'Cheia de activare nu este corectă sau a expirat. Te rugăm să verifici email-ul din nou şi să urmăreşti instrucţiunile.';
+$lang['auth_message_new_email_sent'] = 'Un email de confirmare a fost trimis către %s. Urmăreşte instrucţiunile din email pentru a putea schimba adresa de email.';
+$lang['auth_message_new_email_activated'] = 'Ai schimbat adresa de email cu succes';
+$lang['auth_message_new_email_failed'] = 'Cheia de activare nu este corectă sau a expirat. Te rugăm să verifici email-ul din nou şi să urmăreşti instrucţiunile.';
+$lang['auth_message_banned'] = 'Contul tău este blocat.';
+$lang['auth_message_unregistered'] = 'Contul tău a fost şters...';
 
 // Email subjects
-$lang['auth_subject_welcome'] = 'Welcome to %s!';
-$lang['auth_subject_activate'] = 'Welcome to %s!';
-$lang['auth_subject_forgot_password'] = 'Forgot your password on %s?';
-$lang['auth_subject_reset_password'] = 'Your new password on %s';
-$lang['auth_subject_change_email'] = 'Your new email address on %s';
+$lang['auth_subject_welcome'] = 'Bine ati venit la %s!';
+$lang['auth_subject_activate'] = 'Bine ati venit la %s!';
+$lang['auth_subject_forgot_password'] = 'Ați uitat parola pe %s?';
+$lang['auth_subject_reset_password'] = 'Noua dvs. parolă pe %s';
+$lang['auth_subject_change_email'] = 'Noua adresa de email pe %s';
 
 
 /* End of file tank_auth_lang.php */
