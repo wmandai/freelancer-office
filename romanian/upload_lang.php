497acc7a0ef4400ff28fcbd832ddf940e4d8303d
Ciomirtan Andrei <refublog@gmail.com>
2015-01-11 13:59:17 +0200

    Update upload_lang.php

--- romanian/upload_lang.php
+++ romanian/upload_lang.php
@@ -1,21 +1,21 @@
 <?php
 
-$lang['upload_userfile_not_set'] = "Unable to find a post variable called userfile.";
-$lang['upload_file_exceeds_limit'] = "The uploaded file exceeds the maximum allowed size in your PHP configuration file.";
-$lang['upload_file_exceeds_form_limit'] = "The uploaded file exceeds the maximum size allowed by the submission form.";
-$lang['upload_file_partial'] = "The file was only partially uploaded.";
-$lang['upload_no_temp_directory'] = "The temporary folder is missing.";
-$lang['upload_unable_to_write_file'] = "The file could not be written to disk.";
-$lang['upload_stopped_by_extension'] = "The file upload was stopped by extension.";
-$lang['upload_no_file_selected'] = "You did not select a file to upload.";
-$lang['upload_invalid_filetype'] = "The filetype you are attempting to upload is not allowed.";
-$lang['upload_invalid_filesize'] = "The file you are attempting to upload is larger than the permitted size.";
-$lang['upload_invalid_dimensions'] = "The image you are attempting to upload exceedes the maximum height or width.";
-$lang['upload_destination_error'] = "A problem was encountered while attempting to move the uploaded file to the final destination.";
-$lang['upload_no_filepath'] = "The upload path does not appear to be valid.";
-$lang['upload_no_file_types'] = "You have not specified any allowed file types.";
-$lang['upload_bad_filename'] = "The file name you submitted already exists on the server.";
-$lang['upload_not_writable'] = "The upload destination folder does not appear to be writable.";
+$lang['upload_userfile_not_set'] = "Imposibil de a găsi o variabilă numita userfile.";
+$lang['upload_file_exceeds_limit'] = "Fișierul încărcat depășește dimensiunea maximă permisă în fișierul de configurare PHP.";
+$lang['upload_file_exceeds_form_limit'] = "Fișierul încărcat depășește dimensiunea maximă permisă de formularul de trimitere.";
+$lang['upload_file_partial'] = "Fisierul a fost doar parțial încărcat.";
+$lang['upload_no_temp_directory'] = "Directorul temporar lipsește.";
+$lang['upload_unable_to_write_file'] = "Fişierul nu a putut fi scris pe disc.";
+$lang['upload_stopped_by_extension'] = "Fişierul încărcat a fost oprit de către extensie.";
+$lang['upload_no_file_selected'] = "Nu ai selectat niciun fişier pentru încărcare";
+$lang['upload_invalid_filetype'] = "Tipul de fişier pe care încercaţi să îl încărcaţi nu este permis";
+$lang['upload_invalid_filesize'] = "Fișierul pe care încercați să îl încărcați este mai mare decât dimensiunea permisă.";
+$lang['upload_invalid_dimensions'] = "Imaginea pe care încercați să încărcați depaseste înălțimea maximă sau lățimea.";
+$lang['upload_destination_error'] = "O problemă a fost întâlnită în timp ce s-a încercat mutarea fișierul încărcat către destinația finală.";
+$lang['upload_no_filepath'] = "Calea de încărcare nu pare să fie validă.";
+$lang['upload_no_file_types'] = "Nu ați specificat niciun tip de fișiere permise.";
+$lang['upload_bad_filename'] = "Numele de fișier pe care l-aţi trimis deja există pe server.";
+$lang['upload_not_writable'] = "Dosarul de destinație pentru încărcare nu pare a avea drept de scriere.";
 
 
 /* End of file upload_lang.php */
