89c845d78e3a8c505110e7b4a4fec83bab830ca8
Ciomirtan Andrei <refublog@gmail.com>
2015-01-11 13:03:51 +0200

    Update migration_lang.php

--- romanian/migration_lang.php
+++ romanian/migration_lang.php
@@ -1,12 +1,12 @@
 <?php
 
-$lang['migration_none_found']			= "No migrations were found.";
-$lang['migration_not_found']			= "This migration could not be found.";
-$lang['migration_multiple_version']		= "This are multiple migrations with the same version number: %d.";
-$lang['migration_class_doesnt_exist']	= "The migration class \"%s\" could not be found.";
-$lang['migration_missing_up_method']	= "The migration class \"%s\" is missing an 'up' method.";
-$lang['migration_missing_down_method']	= "The migration class \"%s\" is missing an 'down' method.";
-$lang['migration_invalid_filename']		= "Migration \"%s\" has an invalid filename.";
+$lang['migration_none_found']			= "Nu au fost găsite migrații.";
+$lang['migration_not_found']			= "Această migrare nu a putut fi găsită.";
+$lang['migration_multiple_version']		= "Acestea sunt multiple migrații cu același număr de versiune: %d.";
+$lang['migration_class_doesnt_exist']	= "Clasa de migrare \"%s\" nu a putut fi gasită.";
+$lang['migration_missing_up_method']	= "Clasa de migrare \"%s\" lipseşte printr-o metodă de tip 'up'.";
+$lang['migration_missing_down_method']	= "Clasa de migrare \"%s\" lipseşte printr-o metodă de tip 'down'.";
+$lang['migration_invalid_filename']		= "Migrarea \"%s\" are un nume de fişier invalid.";
 
 
 /* End of file migration_lang.php */
