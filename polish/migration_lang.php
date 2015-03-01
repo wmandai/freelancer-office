2c0c4abe68d25c94e036cea1d92c22f9df1aee3f
Florian Gareis <mail@zoker.me>
2015-03-01 11:53:52 +0100

    Update lang to beta 1.6.2#2

--- polish/migration_lang.php
+++ polish/migration_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['migration_none_found']			= "No migrations were found.";
-$lang['migration_not_found']			= "This migration could not be found.";
-$lang['migration_multiple_version']		= "This are multiple migrations with the same version number: %d.";
-$lang['migration_class_doesnt_exist']	= "The migration class \"%s\" could not be found.";
-$lang['migration_missing_up_method']	= "The migration class \"%s\" is missing an 'up' method.";
-$lang['migration_missing_down_method']	= "The migration class \"%s\" is missing an 'down' method.";
-$lang['migration_invalid_filename']		= "Migration \"%s\" has an invalid filename.";
+$lang['migration_none_found']			= "Nie znaleziono żadnych migracji.";
+$lang['migration_not_found']			= "Nie znaleziono migracji.";
+$lang['migration_multiple_version']		= "Jest kilka migracji o tym samym numerze: %d.";
+$lang['migration_class_doesnt_exist']	= "Nie można odnaleźć klasy migracji \"%s\".";
+$lang['migration_missing_up_method']	= "Klasa migracji \"%s\" nie zawiera metody 'up'.";
+$lang['migration_missing_down_method']	= "Klasa migracji \"%s\" nie zawiera metody 'down'.";
+$lang['migration_invalid_filename']		= "Migracja \"%s\" ma niewłaściwą nazwę.";
 
 
 /* End of file migration_lang.php */
-/* Location: ./system/language/english/migration_lang.php */
+/* Location: ./system/language/polish/migration_lang.php */
