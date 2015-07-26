f2e882729d8c64ad975cee0abdd8cf9ed808cb85
dannyec <danny@ecuachina.com>
2015-07-26 18:49:32 -0500

    Update ftp_lang.php

--- chinese/ftp_lang.php
+++ chinese/ftp_lang.php
@@ -1,17 +1,17 @@
 <?php
 
-$lang['ftp_no_connection']			= "Unable to locate a valid connection ID. Please make sure you are connected before peforming any file routines.";
-$lang['ftp_unable_to_connect']		= "Unable to connect to your FTP server using the supplied hostname.";
-$lang['ftp_unable_to_login']		= "Unable to login to your FTP server. Please check your username and password.";
-$lang['ftp_unable_to_makdir']		= "Unable to create the directory you have specified.";
-$lang['ftp_unable_to_changedir']	= "Unable to change directories.";
-$lang['ftp_unable_to_chmod']		= "Unable to set file permissions. Please check your path. Note: This feature is only available in PHP 5 or higher.";
-$lang['ftp_unable_to_upload']		= "Unable to upload the specified file. Please check your path.";
-$lang['ftp_unable_to_download']		= "Unable to download the specified file. Please check your path.";
-$lang['ftp_no_source_file']			= "Unable to locate the source file. Please check your path.";
-$lang['ftp_unable_to_rename']		= "Unable to rename the file.";
-$lang['ftp_unable_to_delete']		= "Unable to delete the file.";
-$lang['ftp_unable_to_move']			= "Unable to move the file. Please make sure the destination directory exists.";
+$lang['ftp_no_connection']			= "无法定位到有效的绑定邮箱，请确定您在执行文件路径前绑定。";
+$lang['ftp_unable_to_connect']		= "用所提供的主机名无法连接到您的FTP服务器。";
+$lang['ftp_unable_to_login']		= "无法登录您的FTP服务器，请检查用户名和密码。";
+$lang['ftp_unable_to_makdir']		= "无法创建您指定的目录。";
+$lang['ftp_unable_to_changedir']	= "无法改变目录。";
+$lang['ftp_unable_to_chmod']		= "无法设定文件许可。请检查路径。注意：该特征仅在PHP5或更高版本中支持。";
+$lang['ftp_unable_to_upload']		= "无法上传指定文件，请检查路径。";
+$lang['ftp_unable_to_download']		= "无法下载指定文件，请检查路径。";
+$lang['ftp_no_source_file']			= "找不到源文件，请检查路径。";
+$lang['ftp_unable_to_rename']		= "无法重命名文件。";
+$lang['ftp_unable_to_delete']		= "检测不到文件。";
+$lang['ftp_unable_to_move']			= "不能移动文件。请确保目标目录存在。";
 
 
 /* End of file ftp_lang.php */
