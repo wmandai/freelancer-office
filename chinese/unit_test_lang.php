963775a0521bd6e748e35ab0e7e3718f396c6098
dannyec <danny@ecuachina.com>
2015-07-27 20:37:13 -0500

    Update unit_test_lang.php

--- chinese/unit_test_lang.php
+++ chinese/unit_test_lang.php
@@ -1,24 +1,24 @@
 <?php
 
-$lang['ut_test_name']		= 'Test Name';
-$lang['ut_test_datatype']	= 'Test Datatype';
-$lang['ut_res_datatype']	= 'Expected Datatype';
-$lang['ut_result']			= 'Result';
-$lang['ut_undefined']		= 'Undefined Test Name';
-$lang['ut_file']			= 'File Name';
-$lang['ut_line']			= 'Line Number';
-$lang['ut_passed']			= 'Passed';
-$lang['ut_failed']			= 'Failed';
-$lang['ut_boolean']			= 'Boolean';
-$lang['ut_integer']			= 'Integer';
-$lang['ut_float']			= 'Float';
-$lang['ut_double']			= 'Float'; // can be the same as float
-$lang['ut_string']			= 'String';
-$lang['ut_array']			= 'Array';
-$lang['ut_object']			= 'Object';
-$lang['ut_resource']		= 'Resource';
-$lang['ut_null']			= 'Null';
-$lang['ut_notes']			= 'Notes';
+$lang['ut_test_name']		= '测试名字';
+$lang['ut_test_datatype']	= '测试数据库';
+$lang['ut_res_datatype']	= '预期数据类型';
+$lang['ut_result']			= '结果';
+$lang['ut_undefined']		= '未定义的测试字';
+$lang['ut_file']			= '文件名';
+$lang['ut_line']			= '行号';
+$lang['ut_passed']			= '通过';
+$lang['ut_failed']			= '失败';
+$lang['ut_boolean']			= '布尔值';
+$lang['ut_integer']			= '整型数';
+$lang['ut_float']			= '浮点数';
+$lang['ut_double']			= '浮点数'; // can be the same as float
+$lang['ut_string']			= '字符串';
+$lang['ut_array']			= '阵列';
+$lang['ut_object']			= '对象';
+$lang['ut_resource']		= '资源';
+$lang['ut_null']			= '空位';
+$lang['ut_notes']			= '注释';
 
 
 /* End of file unit_test_lang.php */
