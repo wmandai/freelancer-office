80331b6fba63fe0c88f7659e3fe2515c06ab94e3
dannyec <danny@ecuachina.com>
2015-07-27 22:12:43 -0500

    Update unit_test_lang.php

--- chinese/unit_test_lang.php
+++ chinese/unit_test_lang.php
@@ -22,4 +22,4 @@ $lang['ut_notes']			= '注释';
 
 
 /* End of file unit_test_lang.php */
-/* Location: ./system/language/english/unit_test_lang.php */
+/* Location: ./system/language/chinese/unit_test_lang.php */
