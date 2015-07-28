786842cce2b3fa169ed066a362ab2a61a27dceb9
dannyec <danny@ecuachina.com>
2015-07-27 19:36:08 -0500

    Update migration_lang.php

--- chinese/migration_lang.php
+++ chinese/migration_lang.php
@@ -1,12 +1,12 @@
 <?php
 
-$lang['migration_none_found']			= "No migrations were found.";
-$lang['migration_not_found']			= "This migration could not be found.";
-$lang['migration_multiple_version']		= "This are multiple migrations with the same version number: %d.";
-$lang['migration_class_doesnt_exist']	= "The migration class \"%s\" could not be found.";
-$lang['migration_missing_up_method']	= "The migration class \"%s\" is missing an 'up' method.";
-$lang['migration_missing_down_method']	= "The migration class \"%s\" is missing an 'down' method.";
-$lang['migration_invalid_filename']		= "Migration \"%s\" has an invalid filename.";
+$lang['migration_none_found']			= "没有发现迁移。";
+$lang['migration_not_found']			= "这个迁移无法发现。";
+$lang['migration_multiple_version']		= "这个多迁移拥有相同的版本号: %d.";
+$lang['migration_class_doesnt_exist']	= "这个迁移类 \"%s\" 无法发现。";
+$lang['migration_missing_up_method']	= "这个迁移类 \"%s\" 缺失一个'up'方法。";
+$lang['migration_missing_down_method']	= "这个迁移类 \"%s\" 缺失一个'down'方法。";
+$lang['migration_invalid_filename']		= "迁移 \"%s\" 的文件名无效。";
 
 
 /* End of file migration_lang.php */
