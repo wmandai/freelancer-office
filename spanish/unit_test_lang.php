3c19fb2a92e38355448d4a6a0773cb1f746c093a
dannyec <danny@ecuachina.com>
2015-08-02 16:00:37 -0500

    Update unit_test_lang.php

--- spanish/unit_test_lang.php
+++ spanish/unit_test_lang.php
@@ -22,4 +22,4 @@ $lang['ut_notes']           = 'Notas';
 
 
 /* End of file unit_test_lang.php */
-/* Location: ./system/language/english/unit_test_lang.php */
+/* Location: ./system/language/spanish/unit_test_lang.php */
