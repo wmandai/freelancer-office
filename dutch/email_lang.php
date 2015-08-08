aef918c129cc7d8d713775622adc529bc54bee22
Herman Slatman <hermanslatman@hotmail.com>
2015-08-08 11:28:08 +0200

    Other files translated in Dutch

--- dutch/email_lang.php
+++ dutch/email_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['email_must_be_array'] = "De email validatie methode moet een array toegewezen hebben.";
-$lang['email_invalid_address'] = "Ongeldig email adres: %s";
+$lang['email_must_be_array'] = "De e-mail validatie methode moet een array toegewezen hebben.";
+$lang['email_invalid_address'] = "Ongeldig e-mailadres: %s";
 $lang['email_attachment_missing'] = "Niet mogelijk om de volgende bijlage te lokaliseren: %s";
 $lang['email_attachment_unreadable'] = "Niet mogelijk om deze bijlage te openen: %s";
 $lang['email_no_recipients'] = "U moet ontvangers vermelden: Aan, Cc, of Bcc";
-$lang['email_send_failure_phpmail'] = "Niet mogelijk om een email te verzenden met PHP mail().  Uw server is mogelijk niet juist geconfigureerd om via deze methode emails te verzenden.";
-$lang['email_send_failure_sendmail'] = "Niet mogelijk om een email te verzenden met PHP Sendmail.  Uw server is mogelijk niet juist geconfigureerd om via deze methode emails te verzenden.";
-$lang['email_send_failure_smtp'] = "Niet mogelijk om een email te verzenden met PHP SMTP.  Uw server is mogelijk niet juist geconfigureerd om via deze methode emails te verzenden.";
+$lang['email_send_failure_phpmail'] = "Niet mogelijk om een e-mail te verzenden met PHP mail().  Uw server is mogelijk niet juist geconfigureerd om via deze methode e-mails te verzenden.";
+$lang['email_send_failure_sendmail'] = "Niet mogelijk om een e-mail te verzenden met PHP Sendmail.  Uw server is mogelijk niet juist geconfigureerd om via deze methode e-mails te verzenden.";
+$lang['email_send_failure_smtp'] = "Niet mogelijk om een email te verzenden met PHP SMTP.  Uw server is mogelijk niet juist geconfigureerd om via deze methode e-mails te verzenden.";
 $lang['email_sent'] = "Uw bericht is succesvol verzonden met het volgende protocol: %s";
 $lang['email_no_socket'] = "Niet mogelijk om een socket te openen naar Sendmail. Controleer uw instellingen.";
 $lang['email_no_hostname'] = "U heeft geen SMTP hostname gedefinieerd.";
