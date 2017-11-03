e7f1567a4e64d84d8fd3576f6d457668f54f3bc5
silvana-tnk <silvana-tnk@users.noreply.github.com>
2017-11-03 15:22:33 +0330

    calendar_lang.php

---
+++ persian/calendar_lang.php
@@ -0,0 +1,50 @@
+<?php
+$lang['cal_su'] = 'یکشنبه';
+$lang['cal_mo'] = 'دوشنبه';
+$lang['cal_tu'] = 'سه شنبه';
+$lang['cal_we'] = 'چهارشنبه';
+$lang['cal_th'] = 'پنجشنبه';
+$lang['cal_fr'] = 'جمعه';
+$lang['cal_sa'] = 'شنبه';
+$lang['cal_sun'] = 'یکشنبه';
+$lang['cal_mon'] = 'دوشنبه';
+$lang['cal_tue'] = 'سه شنبه';
+$lang['cal_wed'] = 'چهارشنبه';
+$lang['cal_thu'] = 'پنجشنبه';
+$lang['cal_fri'] = 'جمعه';
+$lang['cal_sat'] = 'شنبه';
+$lang['cal_sunday'] = 'یکشنبه';
+$lang['cal_monday'] = 'دوشنبه';
+$lang['cal_tuesday'] = 'سه شنبه';
+$lang['cal_wednesday'] = 'چهارشنبه';
+$lang['cal_thursday'] = 'پنجشنبه';
+$lang['cal_friday'] = 'جمعه';
+$lang['cal_saturday'] = 'شنبه';
+$lang['cal_jan'] = 'Jan';
+$lang['cal_feb'] = 'Feb';
+$lang['cal_mar'] = 'Mar';
+$lang['cal_apr'] = 'Apr';
+$lang['cal_may'] = 'May';
+$lang['cal_jun'] = 'Jun';
+$lang['cal_jul'] = 'Jul';
+$lang['cal_aug'] = 'Aug';
+$lang['cal_sep'] = 'Sep';
+$lang['cal_oct'] = 'Oct';
+$lang['cal_nov'] = 'Nov';
+$lang['cal_dec'] = 'Dec';
+$lang['cal_january'] = 'January';
+$lang['cal_february'] = 'February';
+$lang['cal_march'] = 'March';
+$lang['cal_april'] = 'April';
+$lang['cal_mayl'] = 'May';
+$lang['cal_june'] = 'June';
+$lang['cal_july'] = 'July';
+$lang['cal_august'] = 'August';
+$lang['cal_september'] = 'September';
+$lang['cal_october'] = 'October';
+$lang['cal_november'] = 'November';
+$lang['cal_december'] = 'December';
+
+
+/* End of file calendar_lang.php */
+/* Location: ./application/language/persian/calendar_lang.php */
