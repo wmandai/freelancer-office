e5c290d21df1dd37e9237b3c90fc3721d6d25347
silvana-tnk <silvana-tnk@users.noreply.github.com>
2017-11-03 15:24:05 +0330

    number_lang.php

---
+++ persian/number_lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['terabyte_abbr'] = 'ترابایت';
+$lang['gigabyte_abbr'] = 'گیگابایت';
+$lang['megabyte_abbr'] = 'مگابایت';
+$lang['kilobyte_abbr'] = 'کیلوبایت';
+$lang['bytes'] = 'بایت';
+
+
+/* End of file number_lang.php */
+/* Location: ./application/language/persian/number_lang.php */
