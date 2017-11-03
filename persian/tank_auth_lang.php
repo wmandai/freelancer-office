bde753da7b86a1f0beebaed9395cbbd8a9111331
silvana-tnk <silvana-tnk@users.noreply.github.com>
2017-11-03 15:24:45 +0330

    tank_auth_lang.php

---
+++ persian/tank_auth_lang.php
@@ -0,0 +1,34 @@
+<?php
+$lang['auth_incorrect_password'] = 'رمزعبور اشتباه است!';
+$lang['auth_incorrect_login'] = 'ورود به سیستم با خطا مواجه شده است!';
+$lang['auth_incorrect_email_or_username'] = 'نام کاربری یا ایمیل اشتباه است!';
+$lang['auth_email_in_use'] = 'ایمیلی که شما معرفی کرده اید قبلا توسط شخص دیگری استفاده شده است. لطفا از ایمیل دیگری استفاده کنید!';
+$lang['auth_username_in_use'] = 'نام کاربری که شما معرفی کرده اید قبلا توسط شخص دیگری استفاده شده است. لطفا از نام کاربری دیگری استفاده کنید!';
+$lang['auth_current_email'] = 'ایمیل حال حاضر شما این است!';
+$lang['auth_incorrect_captcha'] = 'کد وارد شده با کد نمایش داده شده در عکس مطابقت ندارد!';
+$lang['auth_captcha_expired'] = 'کد تایید شما منقضی شده است. لطفا دوباره امتحان کنید';
+$lang['auth_message_logged_out'] = 'شما با موفقیت وارد سیستم شده اید!';
+$lang['auth_message_registration_disabled'] = 'ثبت نام در سیستم موقتا مسدود شده است!';
+$lang['auth_message_registration_completed_1'] = 'ثبت نام در سیستم انجام شد! لطفا ایمیل خود را جهت تایید حساب فعال کنید.';
+$lang['auth_message_registration_completed_2'] = 'شما با موفقیت ثبت نام کرده اید!';
+$lang['auth_message_activation_email_sent'] = 'A new activation email has been sent to %s! Follow the instructions in the email to activate your account.';
+$lang['auth_message_activation_completed'] = 'اکانت کاربری شما فعال گردید.';
+$lang['auth_message_activation_failed'] = 'کد فعال سازی وارد شده معتبر نمی باشد!';
+$lang['auth_message_password_changed'] = 'رمز عبور شما تغییر یافت!';
+$lang['auth_message_new_password_sent'] = 'An email with instructions for creating a new password has been sent to you!';
+$lang['auth_message_new_password_activated'] = 'You have successfully reset your password!';
+$lang['auth_message_new_password_failed'] = 'Your activation key is incorrect or expired! Please check your email again and follow the instructions.';
+$lang['auth_message_new_email_sent'] = 'A confirmation email has been sent to %s! Follow the instructions in the email to complete this change of email address.';
+$lang['auth_message_new_email_activated'] = 'You have successfully changed your email!';
+$lang['auth_message_new_email_failed'] = 'Your activation key is incorrect or expired! Please check your email again and follow the instructions.';
+$lang['auth_message_banned'] = 'اکانت شما مسدود شده است!';
+$lang['auth_message_unregistered'] = 'اکانت کاربری شما مسدود شده است...';
+$lang['auth_subject_welcome'] = 'خوش آمدید به %s !';
+$lang['auth_subject_activate'] = 'خوش آمدید به %s !';
+$lang['auth_subject_forgot_password'] = 'Forgot your password on %s?';
+$lang['auth_subject_reset_password'] = 'Your new password on %s.';
+$lang['auth_subject_change_email'] = 'Your new email address on %s.';
+
+
+/* End of file tank_auth_lang.php */
+/* Location: ./application/language/persian/tank_auth_lang.php */
